1e0bbe3543c91ac66fe2eb8fe50b8413298d1e4c
gchalme <gab.cht@icloud.com>
2026-02-23 20:09:39 +0100

    add a small bus

---
+++ bus.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bus.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 22:02:53 by gabch             #+#    #+#             */
+/*   Updated: 2026/02/23 20:09:14 by gabch            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bus.h"
+#include <stdlib.h>
+
+uint8_t	write_bus(uint16_t addr)
+{
+	if (addr < 0x0800) // 2KB internal RAM
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x2000) // Mirror of 0x0000 0x07FF
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x2008) // NES PPU registers
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x4000) // Mirrors of $2000–$2007 (repeats every 8 bytes) 
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x4018) // NES APU and I/O registers
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x4020) // NES APU and I/O registers
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x8000) // idk
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x8000) // idk
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	if (addr <= 0xFFFF)
+	{
+		
+	}
+}
