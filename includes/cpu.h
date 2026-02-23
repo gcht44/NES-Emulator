491b0da83bc4ae72d904e5154f4fa6f14a43f5fe
gchalme <gab.cht@icloud.com>
2026-02-23 21:00:59 +0100

    add sr define

--- includes/cpu.h
+++ includes/cpu.h
@@ -6,7 +6,7 @@
 /*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 17:07:32 by gabch             #+#    #+#             */
-/*   Updated: 2026/02/23 20:54:01 by gabch            ###   ########.fr       */
+/*   Updated: 2026/02/23 21:00:26 by gabch            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,10 @@
 # define O_FLAG(sr)  (sr & 0x40) >> 6 // Overflow flag
 # define N_FLAG(sr)  (sr & 0x80) >> 7 // Negative flag
 
+# define DEFINE_CY(value) value > 0xFF ? 1 : 0
+# define DEFINE_Z(value) value == 0x00 ? 1 : 0
+// # define DEFINE_O(value) (value ^ A) & (value ^ memory) & $80
+# define DEFINE_N(value) (value & 0x80) > 0x00 ? 1 : 0
 
 typedef struct s_cpu
 {
