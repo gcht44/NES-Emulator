391456c76298c14634dcd02a30ceb3f410dbaeb5
gchalme <gab.cht@icloud.com>
2026-02-23 20:10:43 +0100

    add a small bus

--- bus.c
+++ bus.c
@@ -6,14 +6,62 @@
 /*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 22:02:53 by gabch             #+#    #+#             */
-/*   Updated: 2026/02/23 20:09:14 by gabch            ###   ########.fr       */
+/*   Updated: 2026/02/23 20:10:21 by gabch            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bus.h"
 #include <stdlib.h>
 
-uint8_t	write_bus(uint16_t addr)
+void	write_bus(uint16_t addr, uint8_t data)
+{
+	if (addr < 0x0800) // 2KB internal RAM
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x2000) // Mirror of 0x0000 0x07FF
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x2008) // NES PPU registers
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x4000) // Mirrors of $2000–$2007 (repeats every 8 bytes) 
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x4018) // NES APU and I/O registers
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x4020) // NES APU and I/O registers
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x8000) // idk
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	else if (addr < 0x8000) // idk
+	{
+		printf("BUS (write): %04X not implemented\n", addr);
+		exit(EXIT_FAILURE);
+	}
+	if (addr <= 0xFFFF)
+	{
+		
+	}
+}
+
+uint8_t	read_bus(uint16_t addr)
 {
 	if (addr < 0x0800) // 2KB internal RAM
 	{
--- includes/bus.h
+++ includes/bus.h
@@ -6,7 +6,7 @@
 /*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 22:03:24 by gabch             #+#    #+#             */
-/*   Updated: 2026/02/22 22:04:50 by gabch            ###   ########.fr       */
+/*   Updated: 2026/02/23 20:10:38 by gabch            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 # define BUS_H
 
 # include <stdint.h>
+
+void	write_bus(uint16_t addr, uint8_t data);
+uint8_t	read_bus(uint16_t addr);
 
 #endif
