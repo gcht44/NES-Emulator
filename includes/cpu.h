6b1be5fd031a7aebcc45291208bd09ef14ab416c
gchalme <gab.cht@icloud.com>
2026-05-23 20:02:14 +0200

    create new file opcode and move sr instruction

--- includes/cpu.h
+++ includes/cpu.h
@@ -6,7 +6,7 @@
 /*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 17:07:32 by gabch             #+#    #+#             */
-/*   Updated: 2026/02/23 21:00:26 by gabch            ###   ########.fr       */
+/*   Updated: 2026/05/23 20:00:55 by gabch            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,6 @@
 # define CPU_H
 
 # include <stdint.h>
-
-# define CY_FLAG(sr) sr & 0x01       // Carry flag
-# define Z_FLAG(sr)  (sr & 0x2) >> 1 // Zero Flag
-# define ID_FLAG(sr) (sr & 0x4) >> 2 // Interrupt disable
-# define D_FLAG(sr)  (sr & 0x8) >> 3 // Decimal
-# define B_FLAG(sr)  (sr & 0x10) >> 4 // B flag
-# define O_FLAG(sr)  (sr & 0x40) >> 6 // Overflow flag
-# define N_FLAG(sr)  (sr & 0x80) >> 7 // Negative flag
-
-# define DEFINE_CY(value) value > 0xFF ? 1 : 0
-# define DEFINE_Z(value) value == 0x00 ? 1 : 0
-// # define DEFINE_O(value) (value ^ A) & (value ^ memory) & $80
-# define DEFINE_N(value) (value & 0x80) > 0x00 ? 1 : 0
 
 typedef struct s_cpu
 {
---
+++ includes/opcode.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opcode.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/23 20:00:44 by gabch             #+#    #+#             */
+/*   Updated: 2026/05/23 20:01:28 by gabch            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OPCODE_H
+# define OPCODE_H
+
+# define CY_FLAG(sr) sr & 0x01       // Carry flag
+# define Z_FLAG(sr)  (sr & 0x2) >> 1 // Zero Flag
+# define ID_FLAG(sr) (sr & 0x4) >> 2 // Interrupt disable
+# define D_FLAG(sr)  (sr & 0x8) >> 3 // Decimal
+# define B_FLAG(sr)  (sr & 0x10) >> 4 // B flag
+# define O_FLAG(sr)  (sr & 0x40) >> 6 // Overflow flag
+# define N_FLAG(sr)  (sr & 0x80) >> 7 // Negative flag
+
+# define DEFINE_CY(value) value > 0xFF ? 1 : 0
+# define DEFINE_Z(value) value == 0x00 ? 1 : 0
+// # define DEFINE_O(value) (value ^ A) & (value ^ memory) & $80
+# define DEFINE_N(value) (value & 0x80) > 0x00 ? 1 : 0
+
+
+
+#endif
