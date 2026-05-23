3476aab5795032d98fed4b6c27f00e5a9419f134
gchalme <gab.cht@icloud.com>
2026-05-23 20:18:55 +0200

    add first opcode adc

--- includes/opcode.h
+++ includes/opcode.h
@@ -6,12 +6,14 @@
 /*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 20:00:44 by gabch             #+#    #+#             */
-/*   Updated: 2026/05/23 20:01:28 by gabch            ###   ########.fr       */
+/*   Updated: 2026/05/23 20:14:08 by gabch            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OPCODE_H
 # define OPCODE_H
+
+# include <stdint.h>
 
 # define CY_FLAG(sr) sr & 0x01       // Carry flag
 # define Z_FLAG(sr)  (sr & 0x2) >> 1 // Zero Flag
@@ -23,7 +25,7 @@
 
 # define DEFINE_CY(value) value > 0xFF ? 1 : 0
 # define DEFINE_Z(value) value == 0x00 ? 1 : 0
-// # define DEFINE_O(value) (value ^ A) & (value ^ memory) & $80
+# define DEFINE_O(value) (value ^ A) & (value ^ memory) & $80
 # define DEFINE_N(value) (value & 0x80) > 0x00 ? 1 : 0
 
 
--- opcode.c
+++ opcode.c
@@ -1,1 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opcode.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/23 20:02:22 by gabch             #+#    #+#             */
+/*   Updated: 2026/05/23 20:18:34 by gabch            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "opcode.h"
+
+// A rajouter Overflow flag
+void	adc(uint8_t *reg_A, uint8_t value, uint8_t c)
+{
+	*reg_A += value + c;
+	DEFINE_CY(*reg_A);
+	DEFINE_Z(*reg_A);
+	DEFINE_N(*reg_A);
+}
