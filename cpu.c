490019150b18852cc936e9825b807c39218907b1
gchalme <gab.cht@icloud.com>
2026-05-28 01:40:35 +0200

    change sr var and add a getter

--- cpu.c
+++ cpu.c
@@ -6,20 +6,31 @@
 /*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 17:08:08 by gabch             #+#    #+#             */
-/*   Updated: 2026/05/26 20:37:33 by gabch            ###   ########.fr       */
+/*   Updated: 2026/05/28 01:39:50 by gabch            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cpu.h"
 #include <stdio.h>
 
+uint8_t	get_sr(t_flags flags)
+{
+	return ((flags.n << 7) | (flags.v << 6) | (1 << 5) | (flags.b << 4) | (flags.d << 3) | (flags.i << 2) | (flags.z << 1) | flags.c);
+}
+
 void	init_cpu(t_cpu *cpu)
 {
 	cpu->a = 0;
 	cpu->pc = 0xFFFC;
 	cpu->sp = 0xFD;
-	cpu->sr = 0b00100100;
+	cpu->flags.i = 1;
+	cpu->flags.c = 0;
+	cpu->flags.z = 0;
+	cpu->flags.d = 0;
+	cpu->flags.b = 0;
+	cpu->flags.v = 0;
+	cpu->flags.n = 0;
 	cpu->x = 0;
 	cpu->y = 0;
-	printf("Initialisation du cpu; A:%02X, PC:%02X, SP:%02X, SR:%02X, X:%02X, Y:%02X\n", cpu->a, cpu->pc, cpu->sp, cpu->sr, cpu->x, cpu->y);
+	printf("Initialisation du cpu; A:%02X, PC:%02X, SP:%02X, SR:%02X, X:%02X, Y:%02X\n", cpu->a, cpu->pc, cpu->sp, get_sr(cpu->flags), cpu->x, cpu->y);
 }
--- includes/cpu.h
+++ includes/cpu.h
@@ -6,7 +6,7 @@
 /*   By: gabch <gabch@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 17:07:32 by gabch             #+#    #+#             */
-/*   Updated: 2026/05/26 20:37:22 by gabch            ###   ########.fr       */
+/*   Updated: 2026/05/28 01:40:13 by gabch            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,29 @@
 
 # include <stdint.h>
 
+typedef struct s_flags
+{
+	uint8_t		c;
+	uint8_t		z;
+	uint8_t		i;
+	uint8_t		d;
+	uint8_t		b;
+	uint8_t		v;
+	uint8_t		n;
+}	t_flags;
+
+
 typedef struct s_cpu
 {
 	uint8_t		a;
 	uint8_t		x;
 	uint8_t		y;
-	uint8_t		sr;
+	t_flags		flags;
 	uint8_t		sp;
 	uint16_t	pc;
 }	t_cpu;
 
 void	init_cpu(t_cpu *cpu);
+uint8_t	get_sr(t_flags flags);
 
 #endif
